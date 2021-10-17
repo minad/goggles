6023ca87b28fa05ebad320c8b9c5887c6dd0f51b
Daniel Mendler <mail@daniel-mendler.de>
2021-10-17 19:31:45 +0200

    Version 0.2

--- goggles.el
+++ goggles.el
@@ -3,7 +3,7 @@
 ;; Author: Daniel Mendler
 ;; Created: 2020
 ;; License: GPL-3.0-or-later
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "26"))
 ;; Homepage: https://github.com/minad/goggles
 
